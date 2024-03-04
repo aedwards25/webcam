839567169446178e55bdcbb2619e6a2a934b3c4d
Angelique Edwards <angie@Angies-MacBook-Pro.local>
2024-03-04 12:30:28 -0500

    finished project

--- scripts.js
+++ scripts.js
@@ -1,122 +1,75 @@
-const video = document.querySelector('.player');
-const audio = document.querySelector('.snap');
-const canvas = document.querySelector('.photo');
-const ctx = canvas.getContext('2d');
-const strip = document.querySelector('.strip');
+var video = document.querySelector('.player');
+var audio = document.querySelector('.snap');
+var strip = document.querySelector('.strip');
 
+var canvas = document.querySelector('.photo');
+var ctx = canvas.getContext('2d');
 
 function getVideo() {
     navigator.mediaDevices.getUserMedia({video: true, audio: false})
-    .then(localMediaStream => {
-        console.log(localMediaStream); /* this accesses the user video data so we can use it on the page) */
-   
-        video.srcObject = localMediaStream;
+    .then((stream) => {
+        var videoTracks = stream.getVideoTracks();
+        console.log(stream); // <-- accesses the user so we can use it on the page
+
+        video.srcObject = stream;
         video.play();
     })
 
     .catch(err => {
-        console.error(`OH NO!!!`, err);
-      })
-} 
-
+        console.log;('something went wrong.', err);
+    });
+}
 
 function paintToCanvas() {
-    Width = video.videoWidthh;
-    Height= videoheight;
+    var width = video.videoWidth;
+    var height = video.videoHeight;
 
     canvas.width = width;
     canvas.height = height;
 
-    return setInterval ( () =>{
+    return setInterval(() => {
         ctx.drawImage(video, 0, 0, width, height);
-   
-        let pixels = ctx.getImageData(0, 0, width, height);
+
+        var pixels = ctx.getImageData(0, 0, width, height);
         pixels = rgbSplit(pixels);
 
         ctx.putImageData(pixels, 0, 0);
     }, 16);
 }
 
-
-
-
-
-
 function takePhoto() {
-   snap.currentTime = 0;
-   snap.play();
+    audio.currentTime = 0;
+    audio.play();
 
-    const data = canvas.toDataURL('image/jpeg');
-    const link = document.createElement('a');
+    var data = canvas.toDataURL('image/jepg');
+    var link = document.createElement('a');
 
     link.href = data;
     link.setAttribute('download', 'handsome');
-    link.innerHTML = `<img src="${data}" alt="Handsome Man" />`;
+    link.innerHTML = `<img src="${data}" alt='handsome man />`;
 
     strip.insertBefore(link, strip.firstChild);
 }
 
 function redEffect(pixels) {
-    for (let i = 0; i < pixels.data.length; i+=4) {
-      pixels.data[i + 0] = pixels.data[i + 0] + 200; // RED
-      pixels.data[i + 1] = pixels.data[i + 1] - 50; // GREEN
-      pixels.data[i + 2] = pixels.data[i + 2] * 0.5; // Blue
+    for (let i = 0; i < pixels.data.length; i += 4) {
+        pixels.data[i + 0] = pixels.data[i + 0] + 200;
+        pixels.data[i + 1] = pixels.data[i + 1] - 50;
+        pixels.data[i + 2] = pixels.data[i + 2] * 0.5;
     }
 
     return pixels;
 }
-   
-   
-function redEffect(pixels) {
-    for (let i = 0; i < pixels.data.length; i+=4) {
-      pixels.data[i + 0] = pixels.data[i + 0] + 200; // RED
-      pixels.data[i + 1] = pixels.data[i + 1] - 50; // GREEN
-      pixels.data[i + 2] = pixels.data[i + 2] * 0.5; // Blue
-    }
 
-    return pixels;
-}
-   
-   
 function rgbSplit(pixels) {
     for (let i = 0; i < pixels.data.length; i+=4) {
-      pixels.data[i - 150] = pixels.data[i + 0]; // RED
-      pixels.data[i + 500] = pixels.data[i + 1]; // GREEN
-      pixels.data[i - 550] = pixels.data[i + 2]; // Blue
+      pixels.data[i - 150] = pixels.data[i + 0];
+      pixels.data[i + 500] = pixels.data[i + 1];
+      pixels.data[i - 550] = pixels.data[i + 2];
     }
 
     return pixels;
 }
    
-   
-function greenScreen(pixels) {
-    const levels = {};
-   
-   
-    document.querySelectorAll('.rgb input').forEach((input) => {
-      levels[input.name] = input.value;
-    });
-   
-   
-    for (i = 0; i < pixels.data.length; i = i + 4) {
-      red = pixels.data[i + 0];
-      green = pixels.data[i + 1];
-      blue = pixels.data[i + 2];
-      alpha = pixels.data[i + 3];
-   
-   
-      if (red >= levels.rmin
-        && green >= levels.gmin
-        && blue >= levels.bmin
-        && red <= levels.rmax
-        && green <= levels.gmax
-        && blue <= levels.bmax) {
-        // take it out!
-        pixels.data[i + 3] = 0;
-      }
-    }
-   
-    return pixels;
-}
-
 getVideo();
+video.addEventListener("canplay", paintToCanvas);
